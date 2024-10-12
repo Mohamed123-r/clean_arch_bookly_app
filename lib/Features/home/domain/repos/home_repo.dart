471f16c54eb2cdbae8c7c5434ce8b9254442466d
mohamed <mezoattalla123@gmail.com>
2024-10-12 15:16:31 +0300

    Add HomeRepo interface for book fetching functionality

---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeatureBooks();
+
+  Future<List<BookEntity>> fetchNewestBooks();
+}
