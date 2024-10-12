bfff8baea8ba8d91e93879de0261a1bb23ccb263
mohamed <mezoattalla123@gmail.com>
2024-10-12 15:28:07 +0300

    Add failure handling and new methods to HomeRepo
    - Introduce `Failure` class to manage error states.
    - Implement `fetchFeatureBooks` and `fetchNewestBooks` methods in `HomeRepo`.
    - Add `dartz` package dependency for handling functional programming constructs.

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeatureBooks();
 
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
