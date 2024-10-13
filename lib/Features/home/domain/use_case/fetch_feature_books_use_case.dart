1ec35b88685cc464078644f75e1056683b48e2f2
mohamed <mezoattalla123@gmail.com>
2024-10-13 17:22:50 +0300

    Refactor FetchFeatureBooksUseCase to extend UseCase and implement call method. Add abstract UseCase class for improved use case standardization.

--- lib/Features/home/domain/use_case/fetch_feature_books_use_case.dart
+++ lib/Features/home/domain/use_case/fetch_feature_books_use_case.dart
@@ -1,14 +1,16 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeatureBooksUseCase {
+class FetchFeatureBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
 
   FetchFeatureBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeatureBooks() {
-    return homeRepo.fetchFeatureBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
+    return await homeRepo.fetchFeatureBooks();
   }
 }
