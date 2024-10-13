64cd8724d7b48886d274dfaabf7b0c36553d691d
mohamed <mezoattalla123@gmail.com>
2024-10-13 20:30:13 +0300

    Add models for book information, including AccessInfo, VolumeInfo, and related classes
    - Create AccessInfo class with properties for country, viewability, and book access details.
    - Implement BookModel class extending BookEntity, encapsulating book details including volumeInfo and saleInfo.
    - Introduce supporting classes: Epub, Pdf, ImageLinks, PanelizationSummary, ReadingModes, SaleInfo, and SearchInfo, each with JSON serialization capabilities.
    - Structure VolumeInfo to contain comprehensive book metadata such as title, authors, publisher, and image links.

---
+++ lib/Features/home/data/models/book_model/Book_model.dart
@@ -0,0 +1,55 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+import 'VolumeInfo.dart';
+import 'SaleInfo.dart';
+import 'AccessInfo.dart';
+import 'SearchInfo.dart';
+
+class BookModel extends BookEntity {
+  BookModel({
+    this.kind,
+    this.id,
+    this.etag,
+    this.selfLink,
+    this.volumeInfo,
+    this.saleInfo,
+    this.accessInfo,
+    this.searchInfo,
+  }) : super(
+            bookId: id!,
+            image: volumeInfo?.imageLinks?.smallThumbnail ?? ' ',
+            title: volumeInfo!.title!,
+            author: volumeInfo.authors!.first,
+            price: 0,
+            rating: volumeInfo.ratingsCount);
+
+  String? kind;
+  String? id;
+  String? etag;
+  String? selfLink;
+  VolumeInfo? volumeInfo;
+  SaleInfo? saleInfo;
+  AccessInfo? accessInfo;
+  SearchInfo? searchInfo;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['kind'] = kind;
+    map['id'] = id;
+    map['etag'] = etag;
+    map['selfLink'] = selfLink;
+    if (volumeInfo != null) {
+      map['volumeInfo'] = volumeInfo?.toJson();
+    }
+    if (saleInfo != null) {
+      map['saleInfo'] = saleInfo?.toJson();
+    }
+    if (accessInfo != null) {
+      map['accessInfo'] = accessInfo?.toJson();
+    }
+    if (searchInfo != null) {
+      map['searchInfo'] = searchInfo?.toJson();
+    }
+    return map;
+  }
+}
