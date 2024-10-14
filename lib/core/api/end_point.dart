b94b7b2f227b0676667d434fb6fa986f3b4f0d85
mohamed <mezoattalla123@gmail.com>
2024-10-14 19:26:36 +0300

    - Update `EndPoint` with new URLs for fetching featured and newest books.

--- lib/core/api/end_point.dart
+++ lib/core/api/end_point.dart
@@ -1,4 +1,6 @@
 class EndPoint {
   static String baseUrl = "https://www.googleapis.com/books/v1/";
+  static String featuredBooks = "volumes?q=featured";
+  static String newsBooks = "volumes?q=newest";
 }
 
