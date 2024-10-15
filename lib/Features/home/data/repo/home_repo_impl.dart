876b6bccb5bbe5d4038a55434b6c456765fb5f73
mohamed <mezoattalla123@gmail.com>
2024-10-15 19:01:18 +0300

    Implement HomeRepoImpl with local and remote data sources for fetching featured books

--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -1,13 +1,34 @@
+import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/error/error_model.dart';
 import 'package:bookly/core/error/exceptions.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
+
   @override
-  Future<Either<ServerException, List<BookEntity>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<ServerException, List<BookEntity>>> fetchFeatureBooks() async {
+    try {
+      var bookList = homeLocalDataSource.fetchFeatureBooks();
+
+      if (bookList.isNotEmpty) {
+        return right(bookList);
+      }
+      var book = await homeRemoteDataSource.fetchFeatureBooks();
+      return right(book);
+    } catch (e) {
+      return left(
+        ServerException(errorModel: ErrorModel.fromJson(e)),
+      );
+    }
   }
 
   @override
