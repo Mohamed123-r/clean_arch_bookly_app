2d0505beecc7bbe4bceba6dfb100e0c991868b74
mohamed <mezoattalla123@gmail.com>
2024-10-16 20:35:12 +0300

    Add fetchFeaturedBooks method to FeaturedBooksCubit
    Implement fetchFeaturedBooks method in FeaturedBooksCubit to load featured books. Import necessary use case and exception classes for handling errors. Emit loading, success, and failure states based on the result of the fetch operation.

--- lib/Features/home/presentation/manage/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manage/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_case/fetch_feature_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+  final FetchFeatureBooksUseCase fetchFeaturedBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailed(error: failure.toString())),
+      (books) => emit(FeaturedBooksSuccess(books: books)),
+    );
+  }
 }
