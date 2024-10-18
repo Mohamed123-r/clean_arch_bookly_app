da81d9bee06e53b692920f8b4475c17a8b210586
mohamed <mezoattalla123@gmail.com>
2024-10-18 19:37:09 +0300

    Refactor FeaturedBooks implementation to use Bloc for state management
    - Replace `meta` package with `flutter_bloc` for improved state management.
    - Modify `FeaturedBooksCubit` to include asynchronous fetching of featured books.
    - Change `FeaturedBooksListView` from Stateless to StatefulWidget to implement infinite scrolling.
    - Implement scroll listener to fetch more books upon reaching the end of the list.
    - Update UI components to handle dynamic book data.

--- lib/Features/home/presentation/manage/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manage/featured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
+
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/use_case/fetch_feature_books_use_case.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
@@ -10,9 +10,9 @@ class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
       : super(FeaturedBooksInitial());
   final FetchFeatureBooksUseCase fetchFeaturedBooksUseCase;
 
-  Future<void> fetchFeaturedBooks() async {
+  Future<void> fetchFeaturedBooks({int pageNum = 0}) async {
     emit(FeaturedBooksLoading());
-    var result = await fetchFeaturedBooksUseCase.call();
+    var result = await fetchFeaturedBooksUseCase.call(pageNum);
     result.fold(
       (failure) => emit(FeaturedBooksFailed(error: failure.toString())),
       (books) => emit(FeaturedBooksSuccess(books: books)),
--- lib/Features/home/presentation/manage/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manage/featured_books_cubit/featured_books_state.dart
@@ -1,6 +1,6 @@
 part of 'featured_books_cubit.dart';
 
-@immutable
+
 abstract class FeaturedBooksState {}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
